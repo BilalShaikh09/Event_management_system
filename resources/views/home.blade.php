07c7946b64ced34589689e09252b323cd1de1934
BilalShaikh09 <bilalshaikh66056@gmail.com>
2022-11-26 22:35:12 -1100

    second commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,35 +2,6 @@
 
 @section('homelayout')
 <main>
-    <div>
-        <div class="slideshow-container">
 
-            <div class="mySlides fade">
-              <div class="numbertext">1 / 3</div>
-              <img src="Img/back1.jpeg" style="width:100%">
-              <div class="text">Caption Text</div>
-            </div>
-            
-            <div class="mySlides fade">
-              <div class="numbertext">2 / 3</div>
-              <img src="Img/back2.webp" style="width:100%">
-              <div class="text">Caption Two</div>
-            </div>
-            
-            <div class="mySlides fade">
-              <div class="numbertext">3 / 3</div>
-              <img src="Img/back3.jpeg" style="width:100%">
-              <div class="text">Caption Three</div>
-            </div>
-            
-            </div>
-            <br>
-            
-            <div style="text-align:center">
-              <span class="dot"></span> 
-              <span class="dot"></span> 
-              <span class="dot"></span> 
-            </div>
-    </div>
 </main>
 @endsection
